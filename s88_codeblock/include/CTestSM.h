e39c460233af6e9faad17bf83f63cc90e6f6a598
truyenspidermanvn <a@vnn.vn>
2017-11-08 14:13:55 +0700

    state machine test

---
+++ s88_codeblock/include/CTestSM.h
@@ -0,0 +1,17 @@
+#ifndef CTESTSM_H
+#define CTESTSM_H
+
+
+class CTestSM
+{
+    public:
+        CTestSM();
+        virtual ~CTestSM();
+
+        void testStateMachine();
+
+    protected:
+    private:
+};
+
+#endif // CTESTSM_H
---
+++ s88_codeblock/src/CTestSM.cpp
@@ -0,0 +1,19 @@
+#include "CTestSM.h"
+
+CTestSM::CTestSM()
+{
+    //ctor
+}
+
+CTestSM::~CTestSM()
+{
+    //dtor
+}
+
+void CTestSM::testStateMachine()
+{
+
+    /*
+
+    */
+}
