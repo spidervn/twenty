066533ac8b976e9e85ebcec94b0fdc6e14ef198b
truyenspidermanvn <a@vnn.vn>
2017-11-26 12:01:32 +0700

    memory api application

---
+++ memoryAccurate/app/CMemAccuUI.h
@@ -0,0 +1,17 @@
+#ifndef CMEMACCUUI_H
+#define CMEMACCUUI_H
+
+
+class CMemAccuUI
+{
+    public:
+        CMemAccuUI();
+        virtual ~CMemAccuUI();
+
+        void run();
+
+    protected:
+    private:
+};
+
+#endif // CMEMACCUUI_H
---
+++ memoryAccurate/app/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "CMemAccuUI.h"
+using namespace std;
+
+int main()
+{
+    CMemAccuUI ui;
+
+    ui.run();
+
+    return 0;
+}
